89b247f01329703aade600c29bc83e8946b862fa
Dennis Gosnell <cdep.illabout@gmail.com>
2023-07-26 14:00:41 +0900

    Add flake

---
+++ volume-1-logical-foundations/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "software-foundations";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlay = final: prev: {
+
+          my-vscode-with-coq = final.vscode-with-extensions.override {
+            vscode = final.vscodium;
+            vscodeExtensions = [ final.vscode-extensions.maximedenes.vscoq ];
+          };
+
+          software-foundations-shell = final.stdenv.mkDerivation {
+            name = "software-foundations-shell";
+            dontUnpack = true;
+            nativeBuildInputs = [ final.coq_8_17 fina.my-vscode-with-coq ];
+            installPhase = "touch $out";
+          };
+        };
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+      in
+      {
+        devShell = pkgs.software-foundations-shell;
+      }
+    );
+}
+
